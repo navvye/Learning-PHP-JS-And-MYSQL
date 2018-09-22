922b3a384392a1b5481bdba606c5c64b7ad8ae5d
navvye <navvyeanand@gmail.com>
2018-09-22 14:12:37 +0530

    Adding Chapter 6 via upload

---
+++ Chapter6 Experiments/array.php
@@ -0,0 +1,7 @@
+<?
+$paper[] = "inkjet";
+$paper[] = "Photocopier";
+$paper[] = "Laser";
+$paper [] = "pen";
+
+print($paper);
---
+++ Chapter6 Experiments/chess-array.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$chessboard = array(
+	array('r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'),
+	array('p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'),
+	array(' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '),
+	array(' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '),
+	array(' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '),
+	array(' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '),
+	array('P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'),
+	array('R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R')
+  );
+
+  echo "<pre>";
+
+  foreach($chessboard as $row)
+{
+echo $chessboard[7][3];
+
+}
+
+  echo "</pre>";
+?>
