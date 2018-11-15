8a685a804f756895797edb2786bde31ee459df49
navvye <navvyeanand@gmail.com>
2018-11-15 17:38:26 +0530

    Add files via upload

---
+++ Chapter14 Experiments/literal&variable.js
@@ -0,0 +1,11 @@
+myname = "Navvye";
+myage = "12";
+document.write("a: " + 42  + "<br>"); // Numeric literal
+document.write("b: " + "Hi"  + "<br>"); // String literal
+
+document.write("c: " + "true"  + "<br>"); // Constant literal
+
+document.write("d: " + myname  + "<br>"); // string variable
+
+
+document.write("e: " + myage  + "<br>"); // Numeric variable
