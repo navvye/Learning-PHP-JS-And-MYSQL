ef78738fa5162f77d7c2e5278dacb4458990ba97
navvye <navvyeanand@gmail.com>
2018-11-15 14:47:10 +0530

    Update fetching-results-one-cell-at-a-time.php

--- Chapter10 Experiments/fetching-results-one-cell-at-a-time.php
+++ Chapter10 Experiments/fetching-results-one-cell-at-a-time.php
@@ -2,9 +2,9 @@
   require_once 'login.php';
   $conn = new mysqli($hn, $un, $pw, $db);
   if ($conn->connect_error) die("Fatal Error");
-
+$createTable = "CREATE TABLE `chess`.`chessData` ( `moveNumber` INT(3) NOT NULL , `san` CHAR(4) NOT NULL , `fen` CHAR(100) NOT NULL , `gameId` CHAR(3) NOT NULL , `fromSquare` CHAR(2) NOT NULL , `toSquare` CHAR(2) NOT NULL , `color` CHAR(1) NOT NULL , `piece` CHAR(1) NOT NULL ) ENGINE = InnoDB;";
   $query  = "SELECT * FROM classics";
-  $result = $conn->query($query);
+  $result = $conn->query($query,$createTable);
   if (!$result) die("Fatal Error");
 
   $rows = $result->num_rows;
@@ -12,17 +12,16 @@
   for ($j = 0 ; $j < $rows ; ++$j)
   {
     $result->data_seek($j);
-    echo 'Author: '   . htmlspecialchars($result->fetch_assoc()['author'])   . '<br>';
+    echo 'moveNumber: '   . htmlspecialchars($result->fetch_assoc()['moveNumber'])   . '<br>';
     $result->data_seek($j);
-    echo 'Title: '    . htmlspecialchars($result->fetch_assoc()['title'])    . '<br>';
+    echo 'san: '    . htmlspecialchars($result->fetch_assoc()['san'])    . '<br>';
     $result->data_seek($j);
-    echo 'Category: ' . htmlspecialchars($result->fetch_assoc()['category']) . '<br>';
+    echo 'fen: ' . htmlspecialchars($result->fetch_assoc()['fen']) . '<br>';
     $result->data_seek($j);
-    echo 'Year: '     . htmlspecialchars($result->fetch_assoc()['year'])     . '<br>';
+    echo 'gameId: '     . htmlspecialchars($result->fetch_assoc()['gameId'])     . '<br>';
     $result->data_seek($j);
-    echo 'ISBN: '     . htmlspecialchars($result->fetch_assoc()['isbn'])     . '<br><br>';
+    echo 'color: '     . htmlspecialchars($result->fetch_assoc()['color'])     . '<br><br>';
   }
 
-  $result->close();
-  $conn->close();
+ 
 ?>
