6667be02ad67f2b098a2779eeccc34dc20e086ce
navvye <navvyeanand@gmail.com>
2018-11-15 14:54:10 +0530

    Update insertion-id-add.php

--- Chapter10 Experiments/insertion-id-add.php
+++ Chapter10 Experiments/insertion-id-add.php
@@ -1,11 +1,10 @@
 <?php
   require_once 'login.php';
-  $conn = new mysqli($hn, $un, $pw, $db);
+  $conn = new mysqli($hostname, $username, $password, $database);
   if ($conn->connect_error) die("Fatal Error");
   
-  $query  = "INSERT INTO cats VALUES(NULL, 'Lynx', 'Stumpy', 5)";
+  $query  = "INSERT INTO chess(san,fen,gameId,color,piece,fromSquare,toSquare) VALUES(e4, 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1P1P/RNBQKBNR w KQkq', '77', 'w','p','e2','e4')";
   $result = $conn->query($query);
-  if (!$result) die ("Database access failed");
+  if (!$result) die ("Database access failed There is an error in the code");
 
-  echo "The Insert ID was: " . $conn->insert_id;
 ?>
